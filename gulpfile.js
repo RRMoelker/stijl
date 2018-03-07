758dce1088869e50a54b388d7ab5566c0a4d3812
Vijay Jawalapersad <info@vjawala.nl>
2018-03-07 19:14:11 +0100

    images should work now

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var globsass = require('gulp-sass-glob');
 var postcss = require('gulp-postcss');
 var concatcss = require('gulp-concat-css');
+var foreach = require('gulp-foreach');
+var path = require('path');
+var replace = require('gulp-replace-path');
 
 
+
+gulp.task('copy-amsterdam-source', function() {
+   return gulp
+       .src('node_modules/gemeente-amsterdam-patterns/source/**/*.*')
+       .pipe(gulp.dest('raw-source'))
+});
 
 gulp.task('build-css', function() {
   return gulp
@@ -13,13 +21,20 @@ gulp.task('build-css', function() {
       includePaths: ['node_modules/']
     }))
     .pipe(concatcss('ams-stijl.css'))
+    .pipe(replace('"/images/', '"../images/'))
     .pipe(gulp.dest('css'))
 });
 
 gulp.task('build-sass', function() {
   return gulp
     .src('src/scss/**/*.scss')
-    .pipe(gulp.dest('scss'))
+    .pipe(foreach(function (stream, file) {
+        var filename = path.parse(file.path).base;
+        return stream
+            .pipe(replace('../../node_modules/gemeente-amsterdam-patterns/source', path.resolve('raw-source')))
+            .pipe(concatcss(filename))
+            .pipe(gulp.dest('scss'))
+    }))
 });
 
 gulp.task('build-images', function() {
@@ -32,4 +47,4 @@ gulp.task('clean', function(){
   return del('tmp/**', {force:true});
 });
 
-gulp.task('default', gulp.series('build-css', 'build-sass', 'build-images'));
+gulp.task('default', gulp.series('copy-amsterdam-source', 'build-css', 'build-sass', 'build-images'));
