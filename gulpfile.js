f3760462896f6aff0e607fb3e1662bfee7798a38
Vijay Jawalapersad <info@vjawala.nl>
2018-03-07 16:26:51 +0100

    most should work, only variables to do

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,34 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var globsass = require('gulp-sass-glob');
 var postcss = require('gulp-postcss');
+var concatcss = require('gulp-concat-css');
 
-gulp.task('default', ['clean', 'build-css']);
 
 
 gulp.task('build-css', function() {
   return gulp
-    .src('styles/**/*.scss')
+    .src('src/css/**/*.scss')
     .pipe(sass({
       includePaths: ['node_modules/']
     }))
-    .pipe(globsass())
-    .pipe(postcss())
-    .pipe(gulp.dest('compiled/css'))
+    .pipe(concatcss('ams-stijl.css'))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('build-sass', function() {
+  return gulp
+    .src('src/scss/**/*.scss')
+    .pipe(gulp.dest('scss'))
+});
+
+gulp.task('build-images', function() {
+  return gulp
+    .src('node_modules/gemeente-amsterdam-patterns/source/images/**/*.*')
+    .pipe(gulp.dest('images'))
 });
 
 gulp.task('clean', function(){
   return del('tmp/**', {force:true});
 });
+
+gulp.task('default', gulp.series('build-css', 'build-sass', 'build-images'));
