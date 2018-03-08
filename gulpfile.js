8830b178f93db7202a433665c12caf29bbf5373e
Vijay Jawalapersad <info@vjawala.nl>
2018-03-08 09:16:17 +0100

    also repace background paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
 var concatcss = require('gulp-concat-css');
 var foreach = require('gulp-foreach');
-var path = require('path');
 var replace = require('gulp-replace-path');
 
 
@@ -22,6 +20,7 @@ gulp.task('build-css', function() {
     }))
     .pipe(concatcss('ams-stijl.css'))
     .pipe(replace(/(background-image: url\(")(.*)(\/images\/)/g, 'background-image: url("../images/'))
+    .pipe(replace(/(background: url\(")(.*)(\/images\/)/g, 'background: url("../images/'))
     .pipe(gulp.dest('css'))
 });
 
@@ -29,7 +28,6 @@ gulp.task('build-sass', function() {
   return gulp
     .src('src/scss/**/*.scss')
     .pipe(foreach(function (stream, file) {
-        var filename = path.parse(file.path).base;
         return stream
             .pipe(replace('../../node_modules/gemeente-amsterdam-patterns/source', '../raw-source'))
             .pipe(gulp.dest('scss'))
